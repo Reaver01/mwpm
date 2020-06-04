925b729af1c72202eb532fffc63d71b5f5e55242
Reaver01 <jawmac@gmail.com>
2020-06-04 14:11:10 -0600

    add configurable settings

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Gives the ability to move all wall endpoints that meet at the same point.
 Just hold alt while you let go of an endpoint you've grabbed, and any other endpoints that were sitting at it's original location will follow right along!
+
+# Configurable Settings:
+- Reverse Behaviour: Reverses key held behavior so that multiple walls move by default, and key must be used to move a single wall.
+- Key Code: Numerical key code to be used. You can use something like https://keycode.info/ to find the code.
+- Pixel Offset: Number of pixels to use as an offset if you want to also be able to grab walls endpoints that are close by.
+- Delete small walls: If both wall endpoints fall within the Pixel Offset, delete the wall instead of grabbing one endpoint.
--- mwpm.js
+++ mwpm.js
@@ -8,7 +8,7 @@ const MWPM = (() => {
         pressedKeys[e.keyCode] = true;
     };
     const hookOnpreUpdateWall = async function (scene, wall, update) {
-        if (update.hasOwnProperty("c") && pressedKeys[18]) {
+        if ((!game.settings.get("mwpm", "reverse") && update.hasOwnProperty("c") && pressedKeys[game.settings.get("mwpm", "key")]) || (game.settings.get("mwpm", "reverse") && update.hasOwnProperty("c") && !pressedKeys[game.settings.get("mwpm", "key")])) {
             updateRunning = true;
             let endpoint = [];
             let newEndpoint = [];
@@ -24,14 +24,24 @@ const MWPM = (() => {
                 endpoint = [wall.c[2], wall.c[3]];
                 newEndpoint = [update.c[2], update.c[3]];
             }
-            let otherWalls = scene.data.walls.filter(w => w._id !== wall._id && ((w.c[0] === endpoint[0] && w.c[1] === endpoint[1]) || (w.c[2] === endpoint[0] && w.c[3] === endpoint[1])));
+            let offset = game.settings.get("mwpm", "offset");
+            let smallWalls = scene.data.walls.filter(w => (offset > 0 && w.c[0] > endpoint[0] - offset && w.c[0] < endpoint[0] + offset && w.c[1] > endpoint[1] - offset && w.c[1] < endpoint[1] + offset) && (offset > 0 && w.c[2] > endpoint[0] - offset && w.c[2] < endpoint[0] + offset && w.c[3] > endpoint[1] - offset && w.c[3] < endpoint[1] + offset));
+            console.log(smallWalls);
+            if (smallWalls.length > 0 && game.settings.get("mwpm", "delete")) {
+                deleteUpdates = [];
+                smallWalls.forEach(small => {
+                    deleteUpdates.push(small._id);
+                });
+                await scene.deleteEmbeddedEntity("Wall", deleteUpdates);
+            }
+            let otherWalls = scene.data.walls.filter(w => w._id !== wall._id && (((w.c[0] === endpoint[0] && w.c[1] === endpoint[1]) || (offset > 0 && w.c[0] > endpoint[0] - offset && w.c[0] < endpoint[0] + offset && w.c[1] > endpoint[1] - offset && w.c[1] < endpoint[1] + offset)) || ((w.c[2] === endpoint[0] && w.c[3] === endpoint[1]) || (offset > 0 && w.c[2] > endpoint[0] - offset && w.c[2] < endpoint[0] + offset && w.c[3] > endpoint[1] - offset && w.c[3] < endpoint[1] + offset))));
             let updates = [];
             otherWalls.forEach(other => {
                 let coords = [];
-                if (other.c[0] === endpoint[0] && other.c[1] === endpoint[1]) {
+                if ((other.c[0] === endpoint[0] && other.c[1] === endpoint[1]) || (offset > 0 && other.c[0] > endpoint[0] - offset && other.c[0] < endpoint[0] + offset && other.c[1] > endpoint[1] - offset && other.c[1] < endpoint[1] + offset)) {
                     coords = [newEndpoint[0], newEndpoint[1], other.c[2], other.c[3]];
                 }
-                if (other.c[2] === endpoint[0] && other.c[3] === endpoint[1]) {
+                if ((other.c[2] === endpoint[0] && other.c[3] === endpoint[1]) || (offset > 0 && other.c[2] > endpoint[0] - offset && other.c[2] < endpoint[0] + offset && other.c[3] > endpoint[1] - offset && other.c[3] < endpoint[1] + offset)) {
                     coords = [other.c[0], other.c[1], newEndpoint[0], newEndpoint[1]];
                 }
                 updates.push({
@@ -49,6 +59,41 @@ const MWPM = (() => {
             if (!updateRunning) {
                 await hookOnpreUpdateWall(scene, wall, update);
             }
+        });
+    });
+
+    Hooks.on("init", function () {
+        game.settings.register("mwpm", "reverse", {
+            name: "Reverse Behaviour",
+            hint: "Reverses key held behavior so that multiple walls move by default, and key must be used to move a single wall.",
+            scope: "world",
+            config: true,
+            default: false,
+            type: Boolean
+        });
+        game.settings.register("mwpm", "key", {
+            name: "Key Code",
+            hint: "Numerical key code to be used. You can use something like https://keycode.info/ to find the code.",
+            scope: "world",
+            config: true,
+            default: 18,
+            type: Number
+        });
+        game.settings.register("mwpm", "offset", {
+            name: "Pixel Offset",
+            hint: "Number of pixels to use as an offset if you want to also be able to grab walls endpoints that are close by.",
+            scope: "world",
+            config: true,
+            default: 0,
+            type: Number
+        });
+        game.settings.register("mwpm", "delete", {
+            name: "Delete small walls",
+            hint: "If both wall endpoints fall within the Pixel Offset, delete the wall instead of grabbing one endpoint.",
+            scope: "world",
+            config: true,
+            default: false,
+            type: Boolean
         });
     });
 })();
